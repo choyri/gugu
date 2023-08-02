bda67f7afade69426b08cc901326d5a0e4fc1d0c
Chotow <i@choyri.com>
2023-08-02 23:17:57 +0800

    WIP

--- user/model.go
+++ user/model.go
@@ -1,7 +1,14 @@
 package user
 
-import "github.com/uptrace/bun"
+import (
+	"time"
+)
 
 type User struct {
-	bun.BaseModel `bun:"table:users,alias:u"`
+	ID          int32  `bun:",pk,autoincrement"`
+	Name        string `bun:",skipupdate" validate:"required_if=ID 0"`
+	DisplayName *string
+	Password    *string
+	CreateTime  time.Time `bun:",nullzero,notnull,default:current_timestamp,skipupdate"`
+	UpdateTime  time.Time `bun:",nullzero,notnull,default:current_timestamp,skipupdate"`
 }
